38a8867236a7e584338a29f64568855a02038d9c
Mahek <mahektasnia@gmail.com>
2026-09-07 23:01:23 +0600

    Add SQL Server EF Core database setup

---
+++ Migrations/20260907165055_InitialCreate.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace student_resource_hub.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
