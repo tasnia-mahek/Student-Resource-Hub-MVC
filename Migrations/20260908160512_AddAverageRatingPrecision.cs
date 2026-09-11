b0237688005d2a888d36b4a2a198878da1c7aa13
Dom1n1cNUB <rabbie.cse.20230104057@aust.edu>
2026-09-11 11:55:02 +0600

    feat(migrations): add entity framework migrations for resource tables and rating precision

---
+++ Migrations/20260908160512_AddAverageRatingPrecision.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace student_resource_hub.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAverageRatingPrecision : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""
+                IF COL_LENGTH(N'dbo.Notes', N'AverageRating') IS NOT NULL
+                BEGIN
+                    ALTER TABLE dbo.Notes ALTER COLUMN AverageRating decimal(3,2) NOT NULL;
+                END
+                """);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "AverageRating",
+                table: "Notes",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(3,2)",
+                oldPrecision: 3,
+                oldScale: 2);
+        }
+    }
+}
